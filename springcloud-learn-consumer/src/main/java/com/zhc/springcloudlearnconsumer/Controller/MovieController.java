e5f0a26437f5dacc982c51dba47f8403bfb1b20c
Zhenghci <344304180@qq.com>
2021-08-05 18:26:11 +0800

    ribbon

--- springcloud-learn-consumer/src/main/java/com/zhc/springcloudlearnconsumer/Controller/MovieController.java
+++ springcloud-learn-consumer/src/main/java/com/zhc/springcloudlearnconsumer/Controller/MovieController.java
@@ -1,6 +1,7 @@
 package com.zhc.springcloudlearnconsumer.Controller;
 
 import com.zhc.springcloudlearnconsumer.entity.User;
+import com.zhc.springcloudlearnconsumer.service.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,19 +12,23 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/movies")
 @RestController
 public class MovieController {
+//    @Autowired
+//    private RestTemplate restTemplate;
+
     @Autowired
-    private RestTemplate restTemplate;
+    private UserFeignClient userFeignClient;
 
     @GetMapping("/users/{id}")
     public User findById(@PathVariable Long id) {
         // 这里用到了RestTemplate的占位符能力
-        User user = this.restTemplate.getForObject("http://microservice-provider-user/users/{id}", User.class, id);
-        // ...电影微服务的业务...
-        return user;
+//        User user = this.restTemplate.getForObject("http://microservice-provider-user/users/{id}", User.class, id);
+//        // ...电影微服务的业务...
+//        return user;
+        return userFeignClient.findById(id);
     }
 
-    @GetMapping("/port")
-    public Integer getPort() {
-        return this.restTemplate.getForObject("http://microservice-provider-user/users/port", Integer.class);
-    }
+//    @GetMapping("/port")
+//    public Integer getPort() {
+//        return this.restTemplate.getForObject("http://microservice-provider-user/users/port", Integer.class);
+//    }
 }
---
+++ springcloud-learn-consumer/src/main/java/com/zhc/springcloudlearnconsumer/service/UserFeignClient.java
@@ -0,0 +1,22 @@
+package com.zhc.springcloudlearnconsumer.service;
+
+import com.zhc.springcloudlearnconsumer.config.UserFeignConfig;
+import com.zhc.springcloudlearnconsumer.entity.User;
+import feign.Logger;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * 对生产者无侵入的写法, 从Resttemplate的命令式变为声明式，怎么做变为做什么
+ */
+@FeignClient(name = "microservice-provider-user")
+public interface UserFeignClient {
+    @GetMapping("/users/{id}")
+    User findById(@PathVariable("id") Long id);
+
+
+
+}
+
